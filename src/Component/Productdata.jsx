94f2e9bb2297270ce1d3cb801fa195dbe820c47e
LightDuck206 <ductotetubqn@gmail.com>
2026-06-09 11:45:47 +0700

    LAB2

---
+++ src/Component/Productdata.jsx
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    name: "Men's Hoodie",
+    price: "450.000 VND",
+    image: "https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=600&h=600&fit=crop",
+    status: "In Stock",
+    badgeBg: "success"
+  },
+  {
+    id: 2,
+    name: "Classic White Sneakers",
+    price: "850.000 VND",
+    image: "https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=600&h=600&fit=crop",
+    status: "Sale -20%",
+    badgeBg: "danger"
+  },
+  {
+    id: 3,
+    name: "Denim Jacket",
+    price: "1.200.000 VND",
+    image: "https://images.unsplash.com/photo-1576995853123-5a10305d93c0?q=80&w=600&h=600&fit=crop",
+    status: "In Stock",
+    badgeBg: "success"
+  },
+  {
+    id: 4,
+    name: "Leather Crossbody Bag",
+    price: "650.000 VND",
+    image: "https://images.unsplash.com/photo-1548036328-c9fa89d128fa?q=80&w=600&h=600&fit=crop",
+    status: "Out of Stock",
+    badgeBg: "secondary"
+  },
+  {
+    id: 5,
+    name: "Graphic T-Shirt",
+    price: "250.000 VND",
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=600&h=600&fit=crop",
+    status: "New Arrival",
+    badgeBg: "info"
+  },
+  {
+    id: 6,
+    name: "Slim Fit Jeans",
+    price: "550.000 VND",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-FQcmqPozYfMtfGiTSlxZFbGm_JJVnIqxcg&s",
+    status: "In Stock",
+    badgeBg: "success"
+  }
+];
